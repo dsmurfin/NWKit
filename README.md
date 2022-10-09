72a209a042f90c0995c746426b98d3c05cf97601
Dan Murfin <dan@dsmurfin.co.uk>
2022-10-09 10:57:15 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NWKit
+
+A description of this package.
---
+++ Tests/NWKitTests/NWKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import NWKit
+
+final class NWKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(NWKit().text, "Hello, World!")
+    }
+}
